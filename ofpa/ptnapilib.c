055a91303cc2fc70a9a412bba4eaee1ceab0edc6
Pioevh <pioevh@163.com>
2016-10-18 16:37:35 +0800

    done

--- ofpa/ptnapilib.c
+++ ofpa/ptnapilib.c
@@ -363,8 +363,7 @@ int ptnapiLldpSignleInquery(char* v_portName)
   t_tempStr=(char*)malloc(def_msgBuff);
   sprintf(t_tempStr,def_ptnapiLldpNeighbour,v_portName);
   logStr("ptnapi Lldp Signle Inquery (command):",1);logStr(t_tempStr,1);
-  ptnapiSocketSessionInquery(g_pSocketLinkList,g_ptnapiSocketFd,def_ptnapiPrompt,t_tempStr,(int)ptnapiLldpData); 
-  pause(); 
+  //ptnapiSocketSessionInquery(g_pSocketLinkList,g_ptnapiSocketFd,def_ptnapiPrompt,t_tempStr,(int)ptnapiLldpData); 
   return(0);  
 }
 
