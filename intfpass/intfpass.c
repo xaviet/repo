83d4d7427778cfe73517faf064697f26285f53d7
Pioevh <pioevh@163.com>
2016-11-08 08:38:59 +0800

    done

--- intfpass/intfpass.c
+++ intfpass/intfpass.c
@@ -16,7 +16,7 @@
 #define def_buffsize 2048
 #define def_namedsize 255
 
-int initSock(char* v_intf,struct ifreq* v_ethreq,struct sockaddr_ll* v_sll)
+int initSock(char* v_intf,struct ifreq* v_ethreq,struct sockaddr_ll* v_sll,struct sockaddr_ll* v_linksll)
 {
   int t_sock=0;
   t_sock=socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
@@ -32,6 +32,8 @@ int initSock(char* v_intf,struct ifreq* v_ethreq,struct sockaddr_ll* v_sll)
   ioctl(t_sock,SIOCGIFFLAGS,v_ethreq);
   ioctl(t_sock,SIOCGIFHWADDR,v_ethreq);
   printf("%s[%d] bind MAC:%02X-%02X-%02X-%02X-%02X-%02X\n",v_intf,t_sock,v_ethreq->ifr_hwaddr.sa_data[0]&0xff,v_ethreq->ifr_hwaddr.sa_data[1]&0xff,v_ethreq->ifr_hwaddr.sa_data[2]&0xff,v_ethreq->ifr_hwaddr.sa_data[3]&0xff,v_ethreq->ifr_hwaddr.sa_data[4]&0xff,v_ethreq->ifr_hwaddr.sa_data[5]&0xff);
+  v_linksll->sll_ifindex=v_ethreq->ifr_ifindex;
+  memcpy(v_linksll->sll_addr,v_ethreq->ifr_hwaddr.sa_data,ETH_ALEN);
   strncpy(v_ethreq->ifr_name,v_intf,IFNAMSIZ);
   if(setsockopt(t_sock,SOL_SOCKET,SO_BINDTODEVICE,(void *)v_ethreq,sizeof(struct ifreq))<0)
   {
@@ -53,45 +55,55 @@ int recvPacket(int v_sock,char* v_buff,struct sockaddr_ll* v_sll)
   {
     printf("sock[%d](ln:%d\t)-DMAC:%02x:%02x:%02x:%02x:%02x:%02x; ",v_sock,t_recvLen,t_eth->h_dest[0],t_eth->h_dest[1],t_eth->h_dest[2],t_eth->h_dest[3],t_eth->h_dest[4],t_eth->h_dest[5]);
     printf("SMAC:%02x:%02x:%02x:%02x:%02x:%02x\n",t_eth->h_source[0],t_eth->h_source[1],t_eth->h_source[2],t_eth->h_source[3],t_eth->h_source[4],t_eth->h_source[5]);
+    return(t_recvLen);
   }
   return(0);
 }
 
 int main(int argc,char **argv)
 {
-  char buffer[def_buffsize]={0};
   int delayUs=1;
   
+  int smac1[6]={0,0,0x22,0x86,0,0};
   char intf1[def_namedsize]="eno16777736";  
   struct ifreq t_ethreq1={0};
   struct sockaddr_ll t_sll1={0};
   int sock1=0;
-  //struct sockaddr_ll destSll1={0};
+  char buffer1[def_buffsize]={0};
+  struct sockaddr_ll linkSll1={0};
+  int len1=0;
+  struct ethhdr* t_eth1=NULL;
   
+  int smac2[6]={0,0,0x22,0x86,0,0x10};
   char intf2[def_namedsize]="eno33554984";
   struct ifreq t_ethreq2={0};
   struct sockaddr_ll t_sll2={0};
   int sock2=0;
-  //struct sockaddr_ll destSll2={0};
+  char buffer2[def_buffsize]={0};
+  struct sockaddr_ll linkSll2={0};
+  int len2=0;
+  struct ethhdr* t_eth2=NULL;
 
   if(argc>=3)
   {
     sscanf(argv[1],"%s",intf1);
-    sscanf(argv[2],"%s",intf2);
-    sscanf(argv[3],"%u",&delayUs);
+    sscanf(argv[2],"%02x:%02x:%02x:%02x:%02x:%02x",&(smac1[0]),&(smac1[1]),&(smac1[2]),&(smac1[3]),&(smac1[4]),&(smac1[5]));
+    sscanf(argv[3],"%s",intf2);
+    sscanf(argv[4],"%02x:%02x:%02x:%02x:%02x:%02x",&(smac2[0]),&(smac2[1]),&(smac2[2]),&(smac2[3]),&(smac2[4]),&(smac2[5]));
+    sscanf(argv[5],"%u",&delayUs);
   }
   else
   {
-    printf("\ncommand notice: intfpass intf1 intf2 delayus\n");
+    printf("\ncommand notice: intfpass intf1 smac1 intf2 smac2 delayus\n");
   }
-  printf("\nDelay: %uus, intf1: %s, intf2: %s\n",delayUs,intf1,intf2);
-  sock1=initSock(intf1,&t_ethreq1,&t_sll1);
+  printf("\nDelay: %uus, intf1: %s[smac-%02x:%02x:%02x:%02x:%02x:%02x], intf2: %s[smac-%02x:%02x:%02x:%02x:%02x:%02x]\n",delayUs,intf1,smac1[0],smac1[1],smac1[2],smac1[3],smac1[4],smac1[5],intf2,smac2[0],smac2[1],smac2[2],smac2[3],smac2[4],smac2[5]);
+  sock1=initSock(intf1,&t_ethreq1,&t_sll1,&linkSll1);
   if(sock1<=0)
   {
     printf("initSock %s fail!\n",intf1);
     return(0);
   }
-  sock2=initSock(intf2,&t_ethreq2,&t_sll2);
+  sock2=initSock(intf2,&t_ethreq2,&t_sll2,&linkSll2);
   if(sock2<=0)
   {
     printf("initSock %s fail!\n",intf2);
@@ -99,9 +111,32 @@ int main(int argc,char **argv)
   }
   while(1) 
   {
-    recvPacket(sock1,buffer,&t_sll1);
-    recvPacket(sock2,buffer,&t_sll2);
-    usleep(delayUs);
+    len1=recvPacket(sock1,buffer1,&t_sll1);
+    t_eth1=(struct ethhdr*)buffer1;
+    len2=recvPacket(sock2,buffer2,&t_sll2);
+    t_eth2=(struct ethhdr*)buffer2;
+    if((len1>0)||(len2>0))
+    {
+      usleep(delayUs);
+      if((len1>0)&&(t_eth1->h_source[0]==(char)(smac1[0]&0xff))
+      &&(t_eth1->h_source[1]==(char)(smac1[1]&0xff))
+      &&(t_eth1->h_source[2]==(char)(smac1[2]&0xff))
+      &&(t_eth1->h_source[3]==(char)(smac1[3]&0xff))
+      &&(t_eth1->h_source[4]==(char)(smac1[4]&0xff))
+      &&(t_eth1->h_source[5]==(char)(smac1[5]&0xff)))
+      {
+        sendto(sock2,buffer1,len1,0,(const struct sockaddr*)&(t_eth1->h_dest),sizeof(t_eth1->h_dest));
+      }
+      if((len2>0)&&(t_eth2->h_source[0]==(char)(smac2[0]&0xff))
+      &&(t_eth2->h_source[1]==(char)(smac2[1]&0xff))
+      &&(t_eth2->h_source[2]==(char)(smac2[2]&0xff))
+      &&(t_eth2->h_source[3]==(char)(smac2[3]&0xff))
+      &&(t_eth2->h_source[4]==(char)(smac2[4]&0xff))
+      &&(t_eth2->h_source[5]==(char)(smac2[5]&0xff)))
+      {
+        sendto(sock1,buffer2,len2,0,(const struct sockaddr*)&(t_eth2->h_dest),sizeof(t_eth2->h_dest));
+      }
+    }
   }
   return(0);
 }
