69122e40bd53ba9a5b5a2054be9580ea91c5440b
Pioevh <pioevh@163.com>
2017-02-06 16:38:15 +0800

    done

--- ofpa/ctrlr/topogen.py
+++ ofpa/ctrlr/topogen.py
@@ -22,7 +22,15 @@ def main(switches,hosts):
     print('    s%d=self.addSwitch(\'s%d\')'%(s,s))
   print('# hosts:')
   for h in range(1,hosts+1):
-    print('    h%d=self.addHost(\'h%d\')'%(h,h))
+    print('    h%d=self.addHost(\'h%d\')'%(h,h))  for x0 in range(1,x+1):
+    for y0 in range(1,y+1):
+      node=((y0-1)*x)+x0
+      if(x0<x):
+        links.append([node,node+1])
+        print('    self.addLink(s%d,s%d)'%(node,node+1))
+      if(y0<y):
+        links.append([node,node+x])
+        print('    self.addLink(s%d,s%d)'%(node,node+x))
   print('# hosts to switches:')
   for l in range(1,(switches if(hosts>switches) else hosts)+1):
     print('    self.addLink(h%d,s%d)'%(l,l))
@@ -57,18 +65,18 @@ def main(switches,hosts):
         else:
           el0.append(-1)
     nm.append(el0)
-  print(nm)
-  str1=''
-  for i in range(1,switches+1):
-    str1+='  %2d '%(i)
-  print(' NdA | NdZ (us) %s'%(str1))
-  print('-'*96)
-  for i in range(switches):
-    str=' %2d  |          '%(i+1)
-    for j in range(switches):
-      str+='%4d '%(nm[i][j])
-    print(str)
-  pass
+  #print(nm)
+  #str1=''
+  #for i in range(1,switches+1):
+    #str1+='  %2d '%(i)
+  #print(' NdA | NdZ (us) %s'%(str1))
+  #print('-'*96)
+  #for i in range(switches):
+    #str=' %2d  |          '%(i+1)
+    #for j in range(switches):
+      #str+='%4d '%(nm[i][j])
+    #print(str)
+  #pass
   return(0)
 
 if(__name__=='__main__'):
