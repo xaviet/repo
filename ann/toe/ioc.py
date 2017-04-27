f8036f9b0726dfcba70f0ab989ce828ddb332944
Pioevh <pioevh@163.com>
2017-04-27 20:08:23 +0800

    done

---
+++ ann/toe/ioc.py
@@ -0,0 +1,17 @@
+## /usr/bin/python3
+## -*- coding: utf-8 -*-
+# g_    global value
+# c_    class value
+# v_    parameter value
+# m_    member value
+# t_    temporary value
+
+'''
+  ioc.py
+  by Pioevh@163.com 2010427
+'''
+
+import commlib
+
+if(__name__=='__main__'):
+  commlib._test_()
